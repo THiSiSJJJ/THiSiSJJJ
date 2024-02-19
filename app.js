975bd518790710b81f8a170deaa4c84a03af4c99
THiSiSJJJ <118153661+THiSiSJJJ@users.noreply.github.com>
2024-02-19 19:29:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const quser = require('./query-user');
 const qlocation = require('./query-location');
 const constants = require('./constants');
